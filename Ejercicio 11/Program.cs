d1b99242893360bc860bf928e5349e36986c43ac
Matias <matiasonores@gmail.com>
2022-06-01 18:18:49 -0300

    Ejercicio 11

--- Ejercicio 11/Program.cs
+++ Ejercicio 11/Program.cs
@@ -14,7 +14,45 @@ namespace Ejercicio_11
          //ingresa un cero y luego mostrar: la cantidad de números primos, la cantidad de
          //números pares, la cantidad de positivos y la cantidad de negativos.
 
+         int n,x=1, negativos = 0, positivos = 0,pares=0,primos=0;
+         bool noPrimo = true;
 
+         Console.WriteLine("Ingrese valores, oprima 0 para salir:");
+
+         n = int.Parse(Console.ReadLine());
+
+         while (n != 0)
+         {
+            if (n % 2 == 0)
+               pares++;
+            else if (n < 0)
+               negativos++;
+
+            else if (n > 0)
+            {
+               positivos++;
+               while (x < n)
+               {
+                  if (n % x == 0)
+                     noPrimo = false;
+
+                  x++;
+               }
+            }
+            if (!noPrimo||n==1)
+            {
+               primos++;
+               
+            }
+            x = 1;
+            noPrimo = true;
+            n = int.Parse(Console.ReadLine());
+         }
+         Console.WriteLine("La cantidad de números negativos fue: {0}", negativos);
+         Console.WriteLine("La cantidad de números positivos fue: {0}", positivos);
+         Console.WriteLine("La cantidad de números pares fue: {0}", pares);
+         Console.WriteLine("La cantidad de números primos fue: {0}", primos);
+         Console.ReadKey();
       }
    }
 }
